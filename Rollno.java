42e48a748e6c4e36d7a0efd2b781ec9c05592ffd
vipinbansal179 <96170702+vipinbansal179@users.noreply.github.com>
2021-12-19 03:25:31 +0530

    Add files via upload

---
+++ Rollno.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+    public class Rollno{
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter ur Name: ");
+            String a = input.nextLine();
+            int b = 2021;
+            System.out.print("Ur Name is = "+a+b+"\n\t"+"2" +"\n\t"+ "1");
+            input.close();
+
+        }
+    }
