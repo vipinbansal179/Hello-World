1555302c36d79402ddf566e970225861ebf8ced5
vipinbansal179 <96170702+vipinbansal179@users.noreply.github.com>
2021-12-19 03:52:33 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+    public class Name{
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter ur Name: ");
+            String a = input.nextLine();
+            System.out.print("Ur Name is = " + a);
+            input.close();
+
+        }
+    }
